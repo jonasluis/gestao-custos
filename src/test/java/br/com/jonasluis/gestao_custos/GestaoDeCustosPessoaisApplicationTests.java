871b6f0b29fe5209b50cdced811da8153b9b83fb
jonasluis <jonasluis66@gmail.com>
2025-06-10 20:36:49 -0300

    :tada: Estrutura Inicial

---
+++ src/test/java/br/com/jonasluis/gestao_custos/GestaoDeCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonasluis.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
